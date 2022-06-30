fb7eb3a1bd462ce0c107f5450d4df5c7d12f52b1
Metta Rolando <mnrolando3@gmail.com>
2022-06-30 14:20:24 -0400

    Completed Edit and Delete

--- albums/views.py
+++ albums/views.py
@@ -27,3 +27,27 @@ def add_album(request):
 def album_detail(request, pk):
     album = get_object_or_404(Album, pk=pk)
     return render(request, "albums/album_detail.html", {"album": album})
+
+
+def edit_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == 'GET':
+        form = AlbumForm(instance=album)
+    else:
+        form = AlbumForm(data=request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect(to='album_detail', pk=pk)
+
+    return render(request, "albums/edit_album.html", {"form": form,
+                  "album": album})
+
+
+def delete_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == 'POST':
+        album.delete()
+        return redirect(to='album_list')
+
+    return render(request, "Albums/delete_Album.html",
+                  {"album": album})
